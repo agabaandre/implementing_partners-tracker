8993a3a414441b56e3606982603d004e7f510e55
AGABA ANDREW <agabaandre@gmail.com>
2022-11-03 16:17:28 +0300

    a

--- application/modules/templates/views/includes/footer.php
+++ application/modules/templates/views/includes/footer.php
@@ -2,14 +2,17 @@
 <!-- /.content-wrapper -->
 
 <footer class="footer">
+    <div class="row" style="text-align:center;">
+        <div class="col-md-12">
+            <p style="font-size:8px; margin:0 auto;">&copy; <?php echo date('Y'); ?>, Ministry of Health -Uganda. <strong>All Rights Reserved</strong></p>
+
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-4">
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/USAID-Identity.svg" style="width:800px; height:80px;">
         </div>
-        <div class="col-md-4">
-            <p style="font-size:8px; margin:0 auto;">&copy; <?php echo date('Y'); ?>, Ministry of Health -Uganda. <strong>All Rights Reserved</strong></p>
 
-        </div>
         <div class="col-md-4">
             <a href="http://health.go.ug" target="_blank"> <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Coat_of_arms_of_Uganda.svg" style="width:800px; height:80px; "> </a>
         </div>
@@ -25,7 +28,6 @@
     <!-- Control sidebar content goes here -->
 </aside>
 <!-- /.control-sidebar -->
-</div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
