ba1faacec88f14b2d9c4d44435854a7b6b34b488
AGABA ANDREW <agabaandre@gmail.com>
2022-11-09 01:45:45 +0300

    fixed auth

--- application/modules/partners/views/form.php
+++ application/modules/partners/views/form.php
@@ -39,7 +39,7 @@
 
 
             <?php
-            if (empty($this->session->userdata('user')->profile_id)) {
+            if ($this->session->userdata('user')->role != "sadmin") {
                 echo form_open_multipart('partners/activities', ['class' => 'search_form']); ?>
                 <div class="form-group col-lg-12">
                     <label>Reporting Project:</label>
